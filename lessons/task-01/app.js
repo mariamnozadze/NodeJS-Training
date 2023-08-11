50aab1d072adaa7e79cbf67b71f3b2b39a258c6b
mariamnozadze <mariamnozadze97@yahoo.com>
2023-08-11 13:31:31 +0400

    - fixed folder structure

---
+++ lessons/task-01/app.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+// fs.writeFileSync("notes.txt", "my name is john cenaaa");
+
+fs.appendFileSync("notes.txt", " wazaaaaap");
+
+// $ node app.js
+
+//that would work also, but stick with convention
+// const files = require('fs');
+// files.writeFileSync('notes.txt', 'my name is john');
