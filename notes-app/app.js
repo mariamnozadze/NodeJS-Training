f08de49596278fab6e137bc9ec8e54220a5c9ee7
mariamnozadze <mariamnozadze97@yahoo.com>
2023-08-24 17:43:27 +0400

    - Argument Parsing with Yargs: Part II

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,47 +1,55 @@
 const chalk = require("chalk");
 const { describe } = require("yargs");
-const yargs = require('yargs');
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
 //customize yargs version
-yargs.version('1.1.0')
+yargs.version("1.1.0");
 
 //add, remove, read, list
 
 //create add command
 yargs.command({
-  command: 'add',
-  describe: 'Add a new note',
-  handler: function(){
-    console.log('Adding a new note')
-  }
-})
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: 'string'
+    },
+  },
+  handler: function (argv) {
+    console.log("title", argv.title);
+  },
+});
 
 //Create remove command
 yargs.command({
-  command: 'add',
-  describe: 'Remove note',
-  handler: function(){
-    console.log('Removing the note')
-  }
-})
+  command: "remove",
+  describe: "Remove note",
+  handler: function () {
+    console.log("Removing the note");
+  },
+});
 
 //Create list command
 yargs.command({
-  command: 'list',
-  describe: 'list note',
-  handler: function(){
-    console.log('Listing the note')
-  }
-})
+  command: "list",
+  describe: "list note",
+  handler: function () {
+    console.log("Listing the note");
+  },
+});
 
 //Create read command
 yargs.command({
-  command: 'read',
-  describe: 'Read note',
-  handler: function(){
-    console.log('Reading the note')
-  }
-})
+  command: "read",
+  describe: "Read note",
+  handler: function () {
+    console.log("Reading the note");
+  },
+});
 
-console.log(yargs.argv)
+// console.log(yargs.argv);
+yargs.parse();
